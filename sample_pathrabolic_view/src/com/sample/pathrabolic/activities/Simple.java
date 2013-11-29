bef670817a7424ddfeda1f6318d385d2dee191a4
Martin <martin.bcu@gmail.com>
2013-11-29 15:15:15 +0900

    Sample first commit

---
+++ sample_pathrabolic_view/src/com/sample/pathrabolic/activities/Simple.java
@@ -0,0 +1,30 @@
+package com.sample.pathrabolic.activities;
+
+import java.util.LinkedHashMap;
+
+import android.os.Bundle;
+
+import com.actionbarsherlock.app.SherlockActivity;
+import com.pathrabolic.PathrabolicView;
+import com.sample.pathrabolic.R;
+
+public class Simple extends SherlockActivity{
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		setTheme(R.style.Theme_Sherlock_Light);
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.simple);
+		
+		PathrabolicView a = (PathrabolicView) findViewById(R.id.pbv);
+		LinkedHashMap<Integer, String> map = new LinkedHashMap<Integer, String>();
+		map.put(0, "first entry");
+		map.put(1, "another button");
+		map.put(2, "yet another button");
+		
+		
+		a.init(map);
+	}
+
+}
